8f61da0713b55d785926cf1feb52c44cc9b47b35
Sanjay Gurav <ilu.reboot@gmail.com>
2025-07-18 19:48:41 +0000

    Check main user (parent) profile is missing from side menubar as discuss

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -46,7 +46,12 @@ export function AppHeader() {
                                 <X/>
                             </Button>
                         </div>
-                        <SheetDescription className="sr-only">Main navigation menu for the application.</SheetDescription>
+                        <SheetDescription asChild>
+                            <div className="flex items-center gap-3 pt-4">
+                                <User className="w-5 h-5" />
+                                <span className="font-medium">Parent User</span>
+                            </div>
+                        </SheetDescription>
                     </SheetHeader>
                     <div className="p-4 pt-0 flex-1 flex flex-col">
                         <Separator />

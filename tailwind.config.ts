978057868e445077f841a0f8f0450cf1cfe677d5
Sanjay Gurav <ilu.reboot@gmail.com>
2025-10-02 10:16:36 +0000

    Check carefully, deeply analyze the error and fix it

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,9 @@
+
 import type {Config} from 'tailwindcss';
 
-const config = {
+const config: Config = {
   darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
@@ -96,6 +96,6 @@ const config = {
     },
   },
   plugins: [require('tailwindcss-animate')],
-} satisfies Config;
+};
 
 export default config;

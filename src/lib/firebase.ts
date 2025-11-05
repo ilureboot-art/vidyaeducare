c3ac07ba28d3f75b0e6d1220642ce9f132534f20
Sanjay Gurav <ilu.reboot@gmail.com>
2025-11-05 02:25:22 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  "projectId": "vidyaeducare",
+  "appId": "1:759861893307:web:9c8d51835795392bc6b19e",
+  "apiKey": "AIzaSyBvwttvsCmg-gL3RBXsxfhHPccIAssXWFo",
+  "authDomain": "vidyaeducare.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "759861893307"
+};
+
+// Initialize Firebase
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };

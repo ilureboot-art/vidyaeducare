64ff5d1e2e1b3f83176725b6eb8bef8c95ceb77e
Sanjay Gurav <ilu.reboot@gmail.com>
2025-11-23 18:17:42 +0000

    Are you sure that firebase error auth configuration not found is resolve

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -1,8 +1,9 @@
+
 'use client';
 
 import { initializeApp, getApp, getApps, type FirebaseApp } from 'firebase/app';
-import { getAuth, connectAuthEmulator, type Auth } from 'firebase/auth';
-import { getFirestore, connectFirestoreEmulator, type Firestore } from 'firebase/firestore';
+import { getAuth, type Auth } from 'firebase/auth';
+import { getFirestore, type Firestore } from 'firebase/firestore';
 import { firebaseConfig } from './config';
 
 let app: FirebaseApp;
@@ -15,17 +16,6 @@ function getFirebase() {
     app = initializeApp(firebaseConfig);
     auth = getAuth(app);
     db = getFirestore(app);
-
-    // Note: The following lines for emulators are typically used for local development.
-    // They are commented out but can be enabled if you set up a local Firebase Emulator Suite.
-    // if (process.env.NODE_ENV === 'development') {
-    //   try {
-    //      connectAuthEmulator(auth, 'http://127.0.0.1:9099', { disableWarnings: true });
-    //      connectFirestoreEmulator(db, '127.0.0.1', 8080);
-    //   } catch (e) {
-    //      console.error('Error connecting to Firebase emulators. Is the emulator suite running?');
-    //   }
-    // }
   } else {
     app = getApp();
     auth = getAuth(app);

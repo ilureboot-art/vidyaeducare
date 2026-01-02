59112063d52d12f29ba7db0d3e718e79bbc5bf56
Sanjay Gurav <ilu.reboot@gmail.com>
2026-01-02 00:51:15 +0000

    // Import the functions you need from the SDKs you need import { initial

--- next.config.js
+++ next.config.js
@@ -20,15 +20,13 @@ const nextConfig = {
   // This is required to make environment variables available in the browser.
   // Note: Only variables prefixed with NEXT_PUBLIC_ will be exposed.
   env: {
-    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    NEXT_PUBLIC_FIREBASE_API_KEY: "AIzaSyCE1-2IqXXp7yBXZ2nu9igTx6f8YDw_N70",
     NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: "vidyaeducare.firebaseapp.com",
     NEXT_PUBLIC_FIREBASE_PROJECT_ID: "vidyaeducare",
-    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: "vidyaeducare.appspot.com",
-    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    NEXT_PUBLIC_FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: "vidyaeducare.firebasestorage.app",
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: "759861893307",
+    NEXT_PUBLIC_FIREBASE_APP_ID: "1:759861893307:web:a3b03e98bee85418c6b19e",
   }
 };
 
 module.exports = nextConfig;
-
-    
